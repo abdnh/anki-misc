9b1cc897b6dac406f84916986fdb3b7e9513a897
Abdo <abd.nh25@gmail.com>
2022-12-07 04:11:20 +0300

    Add reveal-in-file-manager add-on

---
+++ reveal_in_file_manager/README.md
@@ -0,0 +1,5 @@
+# reveal-in-file-manager
+
+This add-on adds a "Reveal in file manager" context menu action in the editor to open the file manager with the clicked image selected.
+
+Should work on Windows and macOS. The add-on will only try to open the file in any other platform supported by Anki like Linux.
---
+++ reveal_in_file_manager/__init__.py
@@ -0,0 +1,48 @@
+import subprocess
+
+from anki.utils import isMac, isWin, noBundledLibs
+from aqt import mw
+from aqt.gui_hooks import editor_will_show_context_menu
+from aqt.qt import *
+from aqt.webview import AnkiWebView
+
+
+# Credit: https://stackoverflow.com/questions/3490336/how-to-reveal-in-finder-or-show-in-explorer-with-qt
+def reveal_in_file_manager(path: str) -> None:
+    if isWin:
+        subprocess.run(f"explorer /select,file://{path}", check=False, shell=True)
+    elif isMac:
+        subprocess.run(
+            [
+                "osascript",
+                "-e",
+                'tell application "Finder"',
+                "-e",
+                "activate",
+                "-e",
+                f'select POSIX file "{path}"',
+                "-e",
+                "end tell",
+            ],
+            check=False,
+        )
+    else:
+        # Just open the file in any other platform
+        with noBundledLibs():
+            QDesktopServices.openUrl(QUrl(f"file://{path}"))
+
+
+def add_reveal_in_file_manager(webview: AnkiWebView, menu: QMenu) -> None:
+    if qtmajor >= 6:
+        context_data = webview.lastContextMenuRequest()
+    else:
+        context_data = webview.page().contextMenuData()
+    url = context_data.mediaUrl()
+    image_name = url.fileName()
+    path = os.path.join(mw.col.media.dir(), image_name)
+    if url.isValid():
+        action = menu.addAction("Reveal in file manager")
+        action.triggered.connect(lambda _, u=path: reveal_in_file_manager(u))
+
+
+editor_will_show_context_menu.append(add_reveal_in_file_manager)
